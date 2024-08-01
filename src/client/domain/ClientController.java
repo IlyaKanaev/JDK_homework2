7b136270955c1d9abb2016d6e080cb42783bdffe
Prios <kanaev.i.v@yandex.ru>
2024-08-01 19:54:29 +0300

    change clientcontroler and clientgui

--- src/client/domain/ClientController.java
+++ src/client/domain/ClientController.java
@@ -3,35 +3,26 @@ package client.domain;
 import client.ui.ClientView;
 import server.domain.ServerController;
 
-/**
- * Класс содержащий логику работы клиента
- *
- * @clientView абстракция графического интерфейса
- * @server объект для связи с сервером
- */
+// Класс содержащий логику работы клиента
 public class ClientController {
-    private boolean connected;
-    private String name;
-    private ClientView clientView;
-    private ServerController serverController;
+    private boolean connected; // вкл-выкл
+    private String name; // то, что юзер сам вписывает при авторизации
+    private ClientView clientView; // абстракция графического интерфейса
+    private ServerController serverController; // объект для связи с сервером
 
+    // в конструкторе - две зависимости, от серверной логики и от интерфейса графики
     public ClientController(ClientView clientView, ServerController serverController) {
         this.clientView = clientView;
         this.serverController = serverController;
         clientView.setClientController(this);
     }
 
-    /**
-     * Метод попытки подключения к серверу. Вызывается из GUI
-     * @param name имя пользователя, которым будем подписывать исходящие сообщения
-     * @return ответ от сервера. Возвращает true, если прошли авторизацию
-     */
     public boolean connectToServer(String name) {
-        this.name = name;
+        this.name = name; // имя пользователя, которым будем подписывать исходящие сообщения
         if (serverController.connectUser(this)){
             showOnWindow("Вы успешно подключились!\n");
             connected = true;
-            String log = serverController.getHistory();
+            String log = serverController.getHistory(); // юзер сразу получает весь текст чата
             if (log != null){
                 showOnWindow(log);
             }
@@ -42,17 +33,13 @@ public class ClientController {
         }
     }
 
-    /**
-     * Метод, с помощью которого сервер передает клиенту сообщения
-     * @param text текст переданный от сервера
-     */
+    // Метод, с помощью которого сервер передает клиенту сообщения
     public void answerFromServer(String text) {
         showOnWindow(text);
     }
 
-    /**
-     * Метод отключения от сервера инициализированное сервером
-     */
+    // Метод отключения от сервера инициализированное сервером (неиспользуемый)
+    // Кроме собственно отключения, меняет состояние поля connected и делает сообщение в текстовом поле окна
     public void disconnectedFromServer() {
         if (connected) {
             connected = false;
@@ -61,17 +48,12 @@ public class ClientController {
         }
     }
 
-    /**
-     * Метод отключения от сервера инициализированное клиентом (например закрыто GUI)
-     */
+//   Метод отключения от сервера инициализированное клиентом (например закрыто GUI)
     public void disconnectFromServer() {
         serverController.disconnectUser(this);
     }
 
-    /**
-     * Метод для передачи сообщения на сервер
-     * @param text текст передаваемого сообщения
-     */
+    // Метод для передачи сообщения на сервер
     public void message(String text) {
         if (connected) {
             if (!text.isEmpty()) {
@@ -82,18 +64,12 @@ public class ClientController {
         }
     }
 
-    /**
-     * Геттер
-     * @return возвращает имя клиента
-     */
+    // Геттер (не используется)
     public String getName() {
         return name;
     }
 
-    /**
-     * Метод вывода сообщения на GUI
-     * @param text текст, который требуется вывести на экран
-     */
+    // Метод вывода сообщения на GUI
     private void showOnWindow(String text) {
         clientView.showMessage(text + "\n");
     }
--- src/client/ui/ClientGUI.java
+++ src/client/ui/ClientGUI.java
@@ -6,17 +6,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-/**
- * Класс описывающий работу графического интерфейса приложения.
- * Является абстракцией GUI
- */
+// Класс описывающий работу графического интерфейса приложения.
 public class ClientGUI extends JFrame implements ClientView {
     private static final int WIDTH = 400;
     private static final int HEIGHT = 300;
 
     private JTextArea log;
-    private JTextField tfIPAddress, tfPort, tfLogin, tfMessage;
-    private JPasswordField password;
+    private JTextField tfIPAddress, tfPort, tfLogin, tfMessage; // tfIPAddress и tfPort не используются
+    private JPasswordField password; // не используется
     private JButton btnLogin, btnSend;
     private JPanel headerPanel;
 
@@ -34,37 +31,28 @@ public class ClientGUI extends JFrame implements ClientView {
         setVisible(true);
     }
 
-    /**
-     * Сеттер
-     * @param clientController объект контроллера, описывающий логику поведения
-     */
+    //Сеттер
     @Override
     public void setClientController(ClientController clientController) {
         this.clientController = clientController;
     }
 
-    /**
-     * Настройка основных параметров GUI
-     */
+    // Настройка основных параметров GUI
     private void setting() {
-        setSize(WIDTH, HEIGHT);
-        setResizable(false);
-        setTitle("Chat client");
-        setDefaultCloseOperation(HIDE_ON_CLOSE);
+        setSize(WIDTH, HEIGHT); // размеры окна
+        setResizable(false); // неизменяемость окошка
+        setTitle("Chat client"); // заголовок
+        setDefaultCloseOperation(HIDE_ON_CLOSE); // основное окно, закрытие которого - остановит программу
     }
 
-    /**
-     * Метод вывода текста на экран GUI. Вызывается из контроллера
-     * @param msg текст, который требуется отобразить на экране
-     */
+    //Метод вывода текста на экран GUI. Вызывается из контроллера
     @Override
     public void showMessage(String msg) {
-        log.append(msg);
+        log.append(msg); // log - это поле в окошке для текста
     }
 
-    /**
-     * Метод, описывающий отключение клиента от сервера со стороны сервера
-     */
+    // Метод, описывающий отключение клиента от сервера со стороны сервера
+    // Вообще не понял. Вроде бы этот метод только делает видимой панель виджетов...
     @Override
     public void disconnectedFromServer(){
         hideHeaderPanel(true);
@@ -87,6 +75,7 @@ public class ClientGUI extends JFrame implements ClientView {
 
     /**
      * Метод, срабатывающий при нажатии кнопки авторизации
+     * Отправляет серверу текст и скрывает видимость панели виджетов
      */
     public void login(){
         if (clientController.connectToServer(tfLogin.getText())){
@@ -94,33 +83,26 @@ public class ClientGUI extends JFrame implements ClientView {
         }
     }
 
-    /**
-     * Метод для отправки сообщения. Используется при нажатии на кнопку send
-     */
+    // Метод для отправки сообщения. Используется при нажатии на кнопку send
     private void message(){
         clientController.message(tfMessage.getText());
         tfMessage.setText("");
     }
 
-    /**
-     * Метод добавления виджетов на экран
-     */
+    // Метод добавления виджетов на экран
     private void createPanel() {
         add(createHeaderPanel(), BorderLayout.NORTH);
         add(createLog());
         add(createFooter(), BorderLayout.SOUTH);
     }
 
-    /**
-     * Метод создания панели авторизации
-     * @return возвращает созданную панель
-     */
+    // Метод создания панели авторизации
     private Component createHeaderPanel() {
-        headerPanel = new JPanel(new GridLayout(2, 3));
-        tfIPAddress = new JTextField("127.0.0.1");
-        tfPort = new JTextField("8189");
+        headerPanel = new JPanel(new GridLayout(2, 3)); // кнопок много, но используются только две
+        tfIPAddress = new JTextField("127.0.0.1"); // текст "от балды"
+        tfPort = new JTextField("8189"); // тоже текст-заглушка
         tfLogin = new JTextField("Ivan Ivanovich");
-        password = new JPasswordField("123456");
+        password = new JPasswordField("123456"); // пароль так себе...
         btnLogin = new JButton("login");
         btnLogin.addActionListener(new ActionListener() {
             @Override
@@ -139,20 +121,15 @@ public class ClientGUI extends JFrame implements ClientView {
         return headerPanel;
     }
 
-    /**
-     * Метод создания центральной панели, на которой отображается история сообщений
-     * @return возвращает созданную панель
-     */
+    // Метод создания центральной панели, на которой отображается история сообщений
     private Component createLog() {
         log = new JTextArea();
         log.setEditable(false);
         return new JScrollPane(log);
     }
 
-    /**
-     * Метод создания панели отправки сообщений
-     * @return возвращает созданную панель
-     */
+    // Метод создания панели отправки сообщений
+    // Непростая магия, надо бы запомнить на будущее
     private Component createFooter() {
         JPanel panel = new JPanel(new BorderLayout());
         tfMessage = new JTextField();
@@ -176,10 +153,8 @@ public class ClientGUI extends JFrame implements ClientView {
         return panel;
     }
 
-    /**
-     * Метод срабатывающий при важных событиях связанных с графическим окном (например окно в фокусе)
-     * @param e  the window event
-     */
+    // Метод срабатывающий при важных событиях связанных с графическим окном (например окно в фокусе)
+    // фокусы-покусы... когда же я так научусь... (вздыхает...)
     @Override
     protected void processWindowEvent(WindowEvent e) {
         super.processWindowEvent(e);
